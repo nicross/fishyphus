16a1f961cdfcf76144e68984072d98365defb799
nicross <nicross@users.noreply.github.com>
2023-04-21 00:12:56 -0500

    Clear all save state with next version

--- src/js/app/updates/v1.0.0.js
+++ src/js/app/updates/v1.0.0.js
@@ -1,7 +1,9 @@
 app.updates.register('1.0.0', () => {
-  clearHighscore()
+  clearAll()
 
-  function clearHighscore() {
+  function clearAll() {
+    app.storage.game.clear()
     app.storage.highscore.clear()
+    app.storage.tutorial.clear()
   }
 })
